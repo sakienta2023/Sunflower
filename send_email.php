045759f1b02383f1c9ab69503c99f266905a7491
Ernest Sakienta <sakienta@gmail.com>
2024-10-26 17:13:24 +0300

    261024_1713

---
+++ send_email.php
@@ -0,0 +1,49 @@
+<?php
+// Include PHPMailer files
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // SMTP settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'sebandeke.94.ronald@gmail.com';
+        $mail->Password = 'September2022';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        // Email settings
+        $mail->setFrom($email, $name);
+        $mail->addAddress('sakienta@gmail.com'); // The email address to receive messages
+
+        // Content of the email
+        $mail->isHTML(true);
+        $mail->Subject = "New Message from $name";
+        $mail->Body    = "<strong>Name:</strong> $name <br>
+                          <strong>Email:</strong> $email <br>
+                          <strong>Message:</strong> <br>" . nl2br($message);
+
+        // Send email
+        if ($mail->send()) {
+            echo "Message sent successfully!";
+        } else {
+            echo "Message could not be sent.";
+        }
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
